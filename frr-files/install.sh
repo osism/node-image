ef5f2869ce9dba8a828821867e1821945fc46d31
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-03-26 10:41:48 +0100

    Get information from bmc configuration
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- frr-files/install.sh
+++ frr-files/install.sh
@@ -3,17 +3,18 @@
 set -x
 
 BASE=/frr-files
-NAME=$(ipmitool user list | awk '/OSISM/ {print substr($2,6)}')
-IP=${NAME:5}
+SYSTEM_HOSTNAME="$(ipmitool dcmi get_mc_id_string|awk '/^.*:/{hostname=$0;gsub(/^(..*): /, "", hostname);printf("%s", hostname); exit}')"
+BMC_IP="$(ipmitool lan print |awk '/^IP Address[ ]*:/{hostname=$0;gsub(/^(..*): /, "", hostname);printf("%s", hostname); exit}')"
+IP_SUFFIX="$(echo "$BMC_IP"|grep -P -oh '\d+$')"
 
-echo Name is: $NAME, IP suffix is: $IP
+echo "Name is: $SYSTEM_HOSTNAME, IP suffix is: $IP_SUFFIX, BMC IP is: $BMC_IP"
 
-echo $NAME > /etc/hostname
-echo "\n - \S - IP - 10.10.21.$IP" > /etc/issue
+echo $SYSTEM_HOSTNAME > /etc/hostname
+echo "\n - \S - IP - 10.10.21.$IP_SUFFIX" > /etc/issue
 
 rm -f /etc/netplan/00-installer-config.yaml
-sed -e "s/IP/$IP/" $BASE/01-osism.yaml > /etc/netplan/01-osism.yaml
-sed -e "s/IP/$IP/" -e "s/HOST/$NAME/" $BASE/frr.conf > /etc/frr/frr.conf
+sed -e "s/IP/$IP_SUFFIX/" $BASE/01-osism.yaml > /etc/netplan/01-osism.yaml
+sed -e "s/IP/$IP_SUFFIX/" -e "s/HOST/$SYSTEM_HOSTNAME/" $BASE/frr.conf > /etc/frr/frr.conf
 cp $BASE/daemons /etc/frr/
 cp $BASE/05-dummy0.netdev /etc/systemd/network/
 

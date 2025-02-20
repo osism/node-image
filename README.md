5be3391ec50727f35d6894e2c7bebfec469ca5b1
Freerk-Ole Zakfeld <fzakfeld@scaleuptech.com>
2025-02-20 22:43:16 +0100

    Add option to convert IPv6 addresses to hex
    Signed-off-by: Freerk-Ole Zakfeld <fzakfeld@scaleuptech.com>

--- README.md
+++ README.md
@@ -117,6 +117,7 @@ interface2_asn: '65404'
 interface2_name: enp2s0f1np1
 ipv4_base: 10.10.21.
 ipv6_base: 'fd0c:cc24:75a0:1:10:10:21:'
+ipv6_hex: 'false'
 layer3_underlay: 'true'
 variant: osism-1
 password_hash: $5$H2wkOHUVMIm2Yl2n$2AR/A2ILtgZcWx5UXL6N56Ha/wkdGvs0w5sFUMQ3iaB
--- contrib/create-image.py
+++ contrib/create-image.py
@@ -182,6 +182,7 @@ def create_context(
         "asn_node_base": "42100210",
         "ipv4_base": "10.10.21.",
         "ipv6_base": "fd0c:cc24:75a0:1:10:10:21:",
+        "ipv6_hex": "false",
         "interface1_name": "enp2s0f0np0",
         "interface2_name": "enp2s0f1np1",
         "interface1_asn": "65405",
